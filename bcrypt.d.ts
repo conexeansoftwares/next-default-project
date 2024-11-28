25c7a87dfbc47922f4dd8d958b03a4f346395ce1
Pedro Rocha <phrocha@apoioprodesp.sp.gov.br>
2024-11-28 02:23:34 -0300

    teste

--- bcrypt.d.ts
+++ bcrypt.d.ts
@@ -1,1 +1,4 @@
-declare module 'bcrypt';
+declare module 'bcrypt' {
+    export function hash(data: string, saltRounds: number): Promise<string>;
+    export function compare(data: string, encrypted: string): Promise<boolean>;
+  }
